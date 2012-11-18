b90d1ff70e782a3ddfa43e5a5b2b1e895858b1a1
musicalbee <paul.ed.shmelev@gmail.com>
2012-11-18 12:30:54 +0400

    first changes to try

--- build.sh
+++ build.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-# Remove Apache2.2 and PHP5.3
-sudo yum -y remove httpd httpd-tools
-sudo yum -y remove php-cli php-common php 
-
 # Install PHP5.4 (with httpd2.4 as dep)
 sudo yum -y install php54* --exclude=php54-mysqlnd 
 
 # Install Nginx
 sudo yum -y install nginx
-sudo rm -rf /etc/nginx/conf.d/*
-sudo chkconfig nginx off
-
 # Install PHP-FPM
 sudo yum -y install php54-fpm 
-sudo chkconfig php-fpm off
-
 # Update all packages
 sudo yum -y update
 
@@ -23,7 +14,6 @@ sudo yum -y update
 sudo mkdir /tmp/build
 sudo git clone git://github.com/rubas/elasticbeanstalk-nginx-php.git /tmp/build
 sudo cp -rf /tmp/build/etc /
-sudo cp -rf /tmp/build/opt /
 
 # Install Composer
 cd /tmp/
@@ -31,20 +21,15 @@ sudo curl -s http://getcomposer.org/installer | php
 sudo mv composer.phar /usr/bin/composer
 
 # Take ownership
-sudo chown -R elasticbeanstalk:elasticbeanstalk /etc/nginx/conf.d \
-                                           /opt/elasticbeanstalk \
+sudo chown -R nginx:nginx /etc/nginx/conf.d \
                                            /usr/bin/composer \
                                            /var/log/nginx \
                                            /var/log/php-fpm
 
-
 # Clear unneeded files
 sudo rm -rf /etc/httpd \
-       /opt/elasticbeanstalk/var/log/* \
+       
        /tmp/build \
        /var/log/httpd \
        /var/log/nginx/* \
        /var/log/php-fpm/*
-
-# Clear root history
-sudo history -c
